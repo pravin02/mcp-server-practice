ffde4a4e01d849a7a1ffccfc306aa37e110600fd
Pravin Patil <prvnpatil11@gmail.com>
2026-04-29 16:04:15 +0530

    feed search MCP server

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-# calculator MCP server
-from fastmcp_calculator import mcp
+from feed_search_mcp_server import mcp
 
-
-def main():
-    # mcp.run() # default it run as STDIO
+def main():    
     mcp.run(transport="http", host="localhost", port=8080, show_banner=True)
-
 
 if __name__ == "__main__":
     main()
+
+
+"""
+To run this applcation all the dependenecies using uv pip install -r pyproject.toml
+and run MCP server using below commands
+python main.py
+
+run model context protocol inspector to test MCP server
+npx @modelcontextprotocol/inspector http://localhost:8080/mcp
+"""
