12d21d17e6fbebeffabca16bab9aff730885c753
Pravin Patil <prvnpatil11@gmail.com>
2026-04-29 14:15:01 +0530

    fastmcp http mcp server

---
+++ fastmcp_http_calculator.py
@@ -0,0 +1,28 @@
+from fastmcp import FastMCP
+
+mcp = FastMCP(name="Calculator")
+
+
+@mcp.tool()
+def addition(no1: float, no2: float) -> float:
+    """Addition of two numbers"""
+    return no1 + no2
+
+
+@mcp.tool()
+def substraction(no1: float, no2: float) -> float:
+    """Substract two numbers"""
+    return no1 - no2
+
+
+@mcp.tool()
+def multiply(no1: float, no2: float) -> float:
+    """Multiply two numbers"""
+    return no1 * no2
+
+
+@mcp.tool(name="division", description="Division of two numbers")
+def division(no1: float, no2: float) -> float:
+    if no2 == 0:
+        raise ValueError("Division number mst not be 0")
+    return no1 / no2
--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 from fastmcp_calculator import mcp
 
 
-def main():    
-    mcp.run() # default it run as STDIO
+def main():
+    # mcp.run() # default it run as STDIO
+    mcp.run(transport="http", host="localhost", port=8080, show_banner=True)
+
 
 if __name__ == "__main__":
     main()
