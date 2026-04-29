c62494f162e0959838fa99eba3f38d9deb1d2203
Pravin Patil <prvnpatil11@gmail.com>
2026-04-29 17:11:33 +0530

    youtubes rrs feeds fetch ability added to feed search MCP server

--- feed_search_mcp_server.py
+++ feed_search_mcp_server.py
@@ -3,17 +3,24 @@ import feedparser
 
 rss_freecodecamp = "https://freecodecamp.org/news/rss/"
 
-mcp = FastMCP(name="Feed Search parser MCP server")
+freecodecamp_youtube_rss_feed = "UC8butISFwT-Wl7EV0hUK0BQ"
 
+rss_youtube = "https://youtube.com/feeds/videos.xml?channel_id="
 
-@mcp.tool()
-def search_feed(query: str, max_results: int = 3):
-    feed = feedparser.parse(rss_freecodecamp)
+mcp = FastMCP(name="Feed Search parser MCP server", version="0.1.0")
+
+@mcp.tool(
+    name="freecodecamp_rss_feed_tool",
+    description="This tool help to pull freecodecamp.org/news rss feeds",
+    tags={"Education", "Information Technology"},
+)
+def freecodecamp_rss_feed_tool(query: str, max_results: int = 3):
+    feed = feedparser.parse(f"{rss_freecodecamp}")
     results = []
     query_lower = query.lower()
     for entry in feed.entries:
         title = entry.title
-        description = entry.description        
+        description = entry.description
         if query_lower in title or query_lower in description:
             results.append(
                 {
@@ -24,13 +31,59 @@ def search_feed(query: str, max_results: int = 3):
             )
             if len(results) >= max_results:
                 break
-    return results or [{"message": "No results found"}]
+    return results or ({"message": "No results found"})
 
+@mcp.tool(
+    name="youtube_rss_feed_tool",
+    description="This tool pulls freecodecamp.org youtube channels videos rss feeds",
+    tags={"Education", "Information Technology"},
+)
+def youtube_rss_feed_tool(query: str, max_results: int = 3):
+    feed = feedparser.parse(f"{rss_youtube}{freecodecamp_youtube_rss_feed}")
+    results = []
+    query_lower = query.lower()
+    for entry in feed.entries:
+        title = entry.title
+        description = entry.description
+        if query_lower in title or query_lower in description:
+            results.append(
+                {
+                    "titel": title,
+                    "description": description,
+                    "url": entry.get("link", ""),
+                }
+            )
+            if len(results) >= max_results:
+                break
+    return results or ({"message": "No results found"})
 
-# def main():
-#     mcp.run(transport="http", host="localhost", port="8080")
-#     # mcp.run() #STDIO
-
-
-# if __name__ == "__main__":
-#     main()
+@mcp.tool(
+    name="youtube_dynamic_rss_feed_tool",
+    description="This tool pulls provided youtube {cahnnel_id} videos rss feeds",
+    tags={"Education", "Information Technology"},
+)
+def youtube_dynamic_rss_feed_tool(channel_id: str, query: str, max_results: int = 3):
+    try:
+        feed_url = f"{rss_youtube}{channel_id}"
+        print(f"Feed Url : {feed_url}")
+        feed = feedparser.parse(feed_url)
+        print(f"No of feeds retrived: {len(feed.entries)}")
+        results = []
+        query_lower = query.lower()
+        for entry in feed.entries:
+            title = entry.title
+            description = entry.description
+            print(f"Title: {title}")
+            if query_lower in title or query_lower in description:
+                results.append(
+                    {
+                        "titel": title,
+                        "description": description,
+                        "url": entry.get("link", ""),
+                    }
+                )
+                if len(results) >= max_results:
+                    break
+        return results or ({"message": "No results found"})
+    except Exception as e:
+        return {"status": False, "error": e}
