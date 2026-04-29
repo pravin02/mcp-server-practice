93a680e4d10d630124bb79dabea188321067678f
Pravin Patil <prvnpatil11@gmail.com>
2026-04-29 12:27:22 +0530

    calculator API implemented

--- fastapi-mcp-calculator.py
+++ fastapi-mcp-calculator.py
@@ -8,6 +8,33 @@ app = FastAPI(title="FastAPI Server")
 def home():
     return {"status", "Fast API server"}
 
+@app.post("/addition")
+def addition(no1 : float, no2: float):
+    """
+    no1 : flaot
+    no2 : float
+    returns addition of two numbers
+    """
+    return {"result": no1 + no2}
+
+@app.post("/substraction")
+def substraction(no1 : float, no2: float):
+    """
+    no1 : flaot
+    no2 : float
+    returns substraction of two numbers
+    """
+    return {"result": no1 - no2}
+
+@app.post("/multiply")
+def multiply(no1 : float, no2: float):
+    """
+    no1 : flaot
+    no2 : float
+    returns multiply of two numbers
+    """
+    return {"result": no1 * no2}
+
 def main():
     uvicorn.run(app, host="localhost", port=8080)
 
